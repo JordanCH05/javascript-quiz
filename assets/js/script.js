19b6a4be4d036c277b610b971689a0a63e1d951a
JordanCH05 <jordi.caristiahill@gmail.com>
2021-08-09 18:01:07 +0000

    Add questions array in script

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+//Questions and answers for quiz grouped by quiz type
+
+let questions = {
+    html:
+    [
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        },
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        }
+    ],
+    css:
+    [
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        },
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        }
+    ],
+    javascript:
+    [
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        },
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        }
+    ],
+    python:
+    [
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        },
+        {
+            question: "",
+            ans1: "",
+            ans2: "",
+            ans3: "",
+            ans4: "",
+            correct: "",
+        }
+    ]
+}
